0fbf5325c15d1eabe607469a8e5bb1d4f818c7a4
TeneoUltrius <teneoultrius@gmail.com>
2023-03-08 23:42:22 +0100

    v3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>HELLO WORLD2!</p>
+        <p>HELLO WORLD3!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
